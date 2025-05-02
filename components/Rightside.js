8752fa09b84a971c1129c3061b016a9681471146
Lionel-Feuga <feuga.lionel1@gmail.com>
2025-05-02 17:27:03 +0200

    t

--- components/Rightside.js
+++ components/Rightside.js
@@ -1,9 +1,13 @@
 import styles from "../styles/Rightside.module.css";
+import Trend from "./Trend"
 
-function RightSide() {
+function RightSide(props) {
   return (
-    <div className={styles.page}>
+    <div className={styles.page} style={props.style}>
       <div><h1 className={styles.h1}>Trends</h1></div>
+      <div>
+        <Trend />
+      </div>
     </div>
   );
 }
---
+++ components/Trend.js
@@ -0,0 +1,15 @@
+import { ST } from "next/dist/shared/lib/utils";
+import styles from "../styles/Trend.module.css";
+
+function Trend() {
+  return (
+    <div className={styles.page}>
+      <div className={styles.trend}>
+        <h4>#hackatweet</h4>
+        <div className={styles.nbrTweets}>2 Tweets</div>
+      </div>
+    </div>
+  );
+}
+
+export default Trend;
