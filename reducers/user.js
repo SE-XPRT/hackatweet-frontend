9c300a80bca27f836d954b5133aa684c1d1c1ecf
SE-XPRT <meknaniimad@gmail.com>
2025-05-02 16:26:45 +0200

    front + redirect

---
+++ reducers/user.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: { token: null, username: null },
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.value.token = action.payload.token;
+      state.value.username = action.payload.username;
+
+      console.log(JSON.parse(JSON.stringify(state.value)));
+    },
+  },
+});
+
+export const { login } = userSlice.actions;
+export default userSlice.reducer;
