1741acc90ddbcfc6570c8cc25f1ea6348187afab
Lionel-Feuga <feuga.lionel1@gmail.com>
2025-05-02 17:32:02 +0200

    persistore

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,37 @@
 import "../styles/globals.css";
 import Head from "next/head";
 import user from "../reducers/user";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+
+const reducers = combineReducers({ user });
+const persistConfig = { key: "hackatweet", storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
-  const store = configureStore({ reducer: { user } });
   return (
     <Provider store={store}>
-      <>
+      <PersistGate persistor={persistor}>
         <Head>
           <title>Next.js App</title>
         </Head>
         <Component {...pageProps} />
-      </>
+      </PersistGate>
     </Provider>
   );
 }
